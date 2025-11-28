5ba3ce9e211bd21b5b5e725f0d142a5340fb5c47
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-11-27 20:44:26 -0500

    v1.1

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,47 @@
 package com.example;
 
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.nio.file.Files;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a, b, c, d;
+        c = 0;
+        c = c;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        while (!sc.hasNextDouble()) {
+            System.out.print("Please enter a valid number: ");
+            sc.next();
+        }
+        a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        while (!sc.hasNextDouble()) {
+            System.out.print("Please enter a valid number: ");
+            sc.next();
+        }
+        b = sc.nextDouble();
+        sc.close();
+
+        double result;
+        try{
+            result = addNumbers(a,b);
+            System.out.print("Result: " + result);
+        }
+        catch(Exception e){
+            throw e;
+        }
+        finally{
+            return;
+        }
+    }
+
+    public static double addNumbers(double x, double y) {
+        return x + y;
     }
 }
